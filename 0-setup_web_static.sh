9dcd4863bbce4ea39cde3cf245a122eaa506e4de
MatiasMtz <4442@holbertonschool.com>
2022-08-25 21:46:08 +0000

    script that sets up the web static deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Script that sets up a web server for the deployment of web_static
+sudo apt-get update -y && sudo apt upgrade -y
+sudo apt-get install nginx -y
+
+if [ ! -d /data/web_static/shared ]
+then
+	mkdir -p /data/web_static/shared
+fi
+if [ ! -d /data/web_static/releases/test/ ]
+then
+	mkdir /data/web_static/releases/test/
+fi
+
+printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tFirst Web Deploy\n\t</body>\n</html>\n" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -hR ubuntu:ubuntu /data
+
+grep -qxF $'\tlocation /hbnb_static/' /etc/nginx/sites-available/default || sed -i '/^\tserver_name/ a\\n\tlocation \/hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+service nginx restart
