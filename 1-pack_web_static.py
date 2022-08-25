abd320ca5ff0c6a2984ec7faddaa5744d8c5ba65
MatiasMtz <4442@holbertonschool.com>
2022-08-25 23:22:52 +0000

    fix 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("verls -l versions/sions") is False:
+        if isdir("versions") is False:
             local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
