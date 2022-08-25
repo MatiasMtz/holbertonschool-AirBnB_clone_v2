e1a3859d399094fa67f6bc83d42fc0286a2bfdb9
MatiasMtz <4442@holbertonschool.com>
2022-08-25 21:57:14 +0000

    fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,14 @@
 # Script that sets up a web server for the deployment of web_static
 sudo apt-get update -y && sudo apt upgrade -y
 sudo apt-get install nginx -y
-#if [ ! -d /data/web_static/shared ]
-#then
-sudo mkdir -p /data/web_static/shared
-#fi
-#if [ ! -d /data/web_static/releases/test/ ]
-#then
-sudo mkdir /data/web_static/releases/test/
-#fi
+if [ ! -d /data/web_static/shared ]
+then
+	sudo mkdir -p /data/web_static/shared
+fi
+if [ ! -d /data/web_static/releases/test/ ]
+then
+	sudo mkdir /data/web_static/releases/test/
+fi
 printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tFirst Web Deploy\n\t</body>\n</html>\n" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
