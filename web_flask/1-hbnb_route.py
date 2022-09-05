f94c1b4354785fcc11087eeaa08a91a01f8392a4
MatiasMtz <4442@holbertonschool.com>
2022-09-05 18:03:05 +0000

    /hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""""
+"""script that starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """"""
+    """displays “Hello HBNB!”"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def holberton():
-    """"""
+    """displays “HBNB”"""
     return 'HBNB'
 
 if __name__ == '__main__':
