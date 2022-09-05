bba7204b5fed3d4d2358854ea3718982131c0b93
MatiasMtz <4442@holbertonschool.com>
2022-09-05 18:04:19 +0000

    /c/<text> route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""""
+"""script that starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,17 +7,17 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """"""
+    """displays “Hello HBNB!”"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def holberton():
-    """"""
+    """displays “HBNB”"""
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """"""
+    """displays “C ” followed by the value of the text variable"""
     return 'C ' + text.replace("_", " ")
 
 if __name__ == '__main__':
