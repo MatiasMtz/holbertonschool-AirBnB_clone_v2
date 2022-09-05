afddd96c87fbfe444ad72b466b7cc386819e2930
MatiasMtz <4442@holbertonschool.com>
2022-09-05 19:18:43 +0000

    changed relationship

--- models/place.py
+++ models/place.py
@@ -7,12 +7,11 @@ from sqlalchemy import Column, ForeignKey, String, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
 place_amenity = Table("place_amenity", Base.metadata, Column("place_id",
-                      String(60), ForeignKey("places.id", onupdate='CASCADE',
-                                             ondelete='CASCADE'),
-                                             primary_key=True),
+                      String(60), ForeignKey("places.id"), primary_key=True,
+                      nullable=False),
                       Column("amenity_id", String(60),
-                             ForeignKey("amenities.id", onupdate='CASCADE',
-                                        ondelete='CASCADE'), primary_key=True))
+                             ForeignKey("amenities.id"), primary_key=True,
+                             nullable=False))
 
 
 class Place(BaseModel, Base):
