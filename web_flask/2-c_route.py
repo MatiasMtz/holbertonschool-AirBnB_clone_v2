9ae3eefccd46b2c9e09a486d95493d692cb9d281
MatiasMtz <4442@holbertonschool.com>
2022-09-05 18:13:01 +0000

    pycodestyle fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,15 +10,18 @@ def hello():
     """displays “Hello HBNB!”"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def holberton():
     """displays “HBNB”"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """displays “C ” followed by the value of the text variable"""
     return 'C ' + text.replace("_", " ")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
